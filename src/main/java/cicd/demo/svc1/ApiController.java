f9af254c87e2bb01d02edef7f0d8de282ffbd778
kst-nhn-com <53928379+kst-nhn-com@users.noreply.github.com>
2021-04-14 11:44:41 +0900

    Update ApiController.java

--- src/main/java/cicd/demo/svc1/ApiController.java
+++ src/main/java/cicd/demo/svc1/ApiController.java
@@ -14,13 +14,13 @@ public class ApiController {
         String res = "";
 
         if (dataId.equals("data11")) {
-            res = "data11";
+            res = "data111";
         } else if (dataId.equals("data12")) {
-            res = "data12";
+            res = "data122";
         } else if (dataId.equals("data13")) {
-            res = "data13";
+            res = "data133";
         } else if (dataId.equals("data14")) {
-            res = "data15";
+            res = "data144";
         } else if (dataId.startsWith("data2")) {
             res = restTemplate.getForObject("http://demo-svc2/data/" + dataId, String.class, "");
         } else if (dataId.startsWith("data3")) {
